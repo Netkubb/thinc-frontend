5eb20519a2724c4560b958e46d1e30a8a9b0beca
maxruayrich <98009085+maxruayrich@users.noreply.github.com>
2023-10-29 03:24:09 +0700

    ใ

--- src/component/home/DescribeCard.jsx
+++ src/component/home/DescribeCard.jsx
@@ -3,37 +3,26 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/material';
-
+//minWidth: 275
 export default function DescribeCard() {
     return (
-        <Card sx={{ minWidth: 275, width: '98vw', height: '70vh', marginX: '1vw', marginTop: '2vh', display: 'flex', alignItems: 'center', justifyContent: 'space-around', backgroundColor: "#1C1C1C", borderRadius: '10px', color: "white" }}>
+        <Card sx={{ minWidth: 275, width: '98vw', height: '70vh', marginX: '1vw', marginTop: '2vh', display: 'flex', alignItems: 'center', justifyContent: 'space-around', backgroundColor: "#1C1C1C", borderRadius: '45px', color: "white" }}>
             <CardContent>
-                {/* <Typography sx={{ fontSize: 20 }} color="whitesmoke" gutterBottom>
-                    Welcome To Our Web Application
-                </Typography> */}
+
                 <Typography sx={{ fontSize: 40 }} color="whitesmoke" gutterBottom>
                     Name : xxx yyy
                 </Typography>
                 <Typography sx={{ fontSize: 40 }} color="whitesmoke" gutterBottom>
                     Position : xxx
                 </Typography>
-                <Typography sx={{ fontSize: 30 }} color="whitesmoke" gutterBottom>
+                <Typography sx={{ fontSize: 30 ,textAlign: 'left'}} color="whitesmoke" gutterBottom>
                     About BokBok
-                </Typography><Typography sx={{ fontSize: 20 }} color="whitesmoke" gutterBottom>
+                </Typography><Typography sx={{ fontSize: 20 ,textAlign: 'left'}} color="whitesmoke" gutterBottom>
                     xxxxxxxx xxxxxxxx
                 </Typography>
-                {/* <Typography variant="h5" component="div">
-                    {"We're Humungousaur"}
-                </Typography> */}
-                {/* <Typography sx={{ my: 1.5 }} color="whitesmoke">
-                    Please Enjoy
-                </Typography>
-                <Typography variant="body1">
-                    This Web App use for ...
-                    <br />
-                    {'"Only for Student of Chulalongkorn University"'}
-                </Typography> */}
+
             </CardContent>
+           
             <Box
                 component="img"
                 sx={{

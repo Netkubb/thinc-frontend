67fddba1963d350e2889c2dad20eda10d7221a5a
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-10-29 00:31:25 +0700

    CREATE REGISTER AND LOGIN API

---
+++ src/api/authAPI/loginAPI.js
@@ -0,0 +1,29 @@
+import Cookies from "js-cookie";
+import { backendIPAddress } from "../../../utils/constants";
+
+export default async function loginAPI(SendUsername, SendPassword) {
+  const response = await fetch(`http://${backendIPAddress}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: SendUsername,
+      password: SendPassword,
+    }),
+  });
+
+  if (!response.ok) {
+    if (response.status === 404) {
+      throw Error("wrong username or password");
+    }
+    throw Error("fail to fetch login");
+  }
+
+  const data = await response.json();
+  const accessToken = data.accessToken;
+
+  Cookies.set("jwt", accessToken, { expires: "3" });
+
+  return { success: true, message: "Login successful" };
+}

6b78607acc05e434dca15701cd6474c0d0f54bac
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-10-30 01:14:54 +0700

    fix hook

--- src/component/feed/feed.jsx
+++ src/component/feed/feed.jsx
@@ -3,17 +3,15 @@ import ButtonInFeed from "./ButtonInFeed";
 import { Box } from "@mui/material";
 import VideoCard from "./VideoCard";
 
-const arr = ['0', '1', '2', '3'];
-var arrSize = arr.length;
-const [index, setIndex] = React.useState(0);
+const arr = ["0", "1", "2", "3"];
+const arrSize = arr.length;
 
 function Feed() {
-  // TODO add feed page
+  const [index, setIndex] = React.useState(0);
   return (
-    <Box display={'flex'} justifyContent={'center'}>
+    <Box display={"flex"} justifyContent={"center"}>
       <ButtonInFeed idx={index} controlidx={setIndex} sizearr={arrSize} />
       <VideoCard idx={index} arr={arr} />
-
     </Box>
   );
 }

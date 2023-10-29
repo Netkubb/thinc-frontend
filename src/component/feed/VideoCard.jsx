0c349c6e95d414b70946066b1b7950ea5e065c39
Netkubb <57309336+Netkubb@users.noreply.github.com>
2023-10-30 02:24:07 +0700

    fetch post

--- src/component/feed/VideoCard.jsx
+++ src/component/feed/VideoCard.jsx
@@ -1,31 +1,55 @@
+/* eslint-disable react/prop-types */
 import * as React from 'react';
 import { Box, Card, CardContent, CardMedia, Typography, Divider, TextField } from '@mui/material';
 import CommentIcon from '@mui/icons-material/Comment';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import DisabledByDefaultIcon from '@mui/icons-material/DisabledByDefault';
 import SendIcon from '@mui/icons-material/Send';
+import getPost from '../../api/postAPI/getPost';
 
-export default function VideoCard(idx, arr) {
+
+export default function VideoCard({idx, arr}) {
+    const [videoURL, setVideoURL] = React.useState('');
+    const [caption, setCaption] = React.useState('');
+    const [likes, setLikes] = React.useState(0);
+    const [comments, setComments] = React.useState([]);
+    const [username, setUsername] = React.useState('');
+
     console.log({ idx } + { arr })
     const [showComment, setShowComment] = React.useState(false);
     const handleCommentClick = () => {
         setShowComment(!showComment)
-
     }
+
+    React.useEffect(() => {
+        if(arr.length === 0) return;
+        if(!arr[idx]) return;
+        const fetchData = async () => {
+            const result = await getPost(arr[idx].id);
+            console.log(result)
+            setVideoURL(result.videoURL);
+            setCaption(result.caption);
+            setLikes(result.like);
+            setComments(JSON.parse(result.comment || '[]'));
+            setUsername(result.username);
+        }
+        fetchData();
+    },[arr,idx])
+
     return (
         <Card sx={{ width: '33vw', height: '87vh', marginX: '1vw', marginTop: '2vh', backgroundColor: "white", borderRadius: '10px', color: "white" }}>
             <CardContent>
                 <Typography variant='inherit' component="div" color={'black'} align='left' fontWeight={'bold'}>
-                    {"Name"}
+                    {username}
                 </Typography>
                 <br></br>
                 <Box sx={{ height: '100px' }}>
                     <Typography variant="caption" component="div" color={'black'} align='left' sx={{ wordBreak: 'break-word' }}>
-                        {'Caption'}
+                        {caption}
                     </Typography>
                 </Box>
                 <Box sx={{ height: '320px', display: 'flex', alignItems: 'center', backgroundColor: "black" }}>
-                    <CardMedia component={'video'} src='/verti.mp4' autoPlay controls
+                    <CardMedia component={'video'} src={videoURL} autoPlay controls
                         sx={{ maxHeight: '300px' }}></CardMedia>
                 </Box>
                 <Box display={'flex'} width={'100%'} height={'200px'} flexDirection={'column-reverse'}>
@@ -33,13 +57,13 @@ export default function VideoCard(idx, arr) {
                         <Box display={'flex'} alignItems={'center'} flexDirection={'column'} onClick={handleCommentClick}>
                             <CommentIcon sx={{ color: 'black' }} />
                             <Typography variant='h9' component="div" color={'black'} align='left'>
-                                {"0"}
+                                {comments.length}
                             </Typography>
                         </Box>
                         <Box display={'flex'} alignItems={'center'} flexDirection={'column'}>
                             <FavoriteIcon sx={{ color: 'black' }} />
                             <Typography variant='inherit' component="div" color={'black'} align='left'>
-                                {"0"}
+                                {likes}
                             </Typography>
                         </Box>
                     </Box>
